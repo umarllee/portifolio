931e9cacd04624007ab232bf85b2c7378c58ed58
umarllee <gulchohreum20631@sabah.edu.az>
2023-07-06 17:23:57 +0400

    ui

--- src/app/public/skills/skills.component.ts
+++ src/app/public/skills/skills.component.ts
@@ -6,5 +6,108 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent {
+  constructor() { }
+
+  perc = 80;
+  skills: any[] = [];
+  skillLibs: any[] = [];
+
+  ngOnInit(): void {
+
+    this.skills = [
+      {
+        name: "HTML",
+        percent: '90%',
+        point: [
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: false },
+        ],
+        countActive: 9,
+      },
+      {
+        name: "CSS",
+        percent: '90%',
+        point: [
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: false },
+        ],
+        countActive: 9,
+      },
+      {
+        name: "JavaScript - TypeScript",
+        percent: '80%',
+        point: [
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: false },
+          { isActive: false },
+        ],
+        countActive: 8,
+      },
+      {
+        name: "Angular",
+        percent: '70%',
+        point: [
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: false },
+          { isActive: false },
+          { isActive: false },
+        ],
+        countActive: 7,
+      },
+      {
+        name: "Git and Github",
+        percent: '90%',
+        point: [
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: true },
+          { isActive: false },
+        ],
+        countActive: 9,
+      },
+    ]
+  }
+
+  // setMyStyles(){
+  //   let styles = {
+  //     'stroke-dashoffset': `calc(440 - (440 * ${deg}) / 100)`,
+  //   };
+  //   return styles;
+  // }
 
 }
